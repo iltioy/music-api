995a597a5bf66b87179028e711c673b2cba590e6
iltioy <tema.illar2@gmail.com>
2023-08-19 05:02:03 +0300

    init

---
+++ src/users/users.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUser() {}
+}
